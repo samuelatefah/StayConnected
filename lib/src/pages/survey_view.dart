c1840f1ba689331cae50d54bd11b4e5de067609b
Samuel Atefah <satefah1@jhu.edu>
2020-09-12 19:33:16 -0400

    added voice chat navigation from surveyView

--- lib/src/pages/survey_view.dart
+++ lib/src/pages/survey_view.dart
@@ -1,4 +1,7 @@
+import 'package:agora_rtc_engine/rtc_engine.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:stay_connected/src/pages/call.dart';
 import 'package:stay_connected/src/pages/index.dart';
 
 class SurveyView extends StatefulWidget {
@@ -7,21 +10,71 @@ class SurveyView extends StatefulWidget {
 }
 
 class _SurveyViewState extends State<SurveyView> {
+  ClientRole _role = ClientRole.Broadcaster;
+
+  /// if channel textField is validated to have error
+  bool _validateError = false;
+  final snackBar = SnackBar(content: Text('Please choose a topic!'));
+
+  String _channelName = '';
+
+  Future<void> onJoin() async {
+    // update input validation
+    setState(() {
+      //_channelController.text.isEmpty
+      _channelName.isEmpty ? _validateError = true : _validateError = false;
+    });
+    //if (_channelController.text.isNotEmpty) {
+    if (_channelName.isNotEmpty) {
+      // await for camera and mic permissions before pushing video page
+      await _handleCameraAndMic();
+      // push video page with given channel name
+      await Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => CallPage(
+            //channelName: _channelController.text,
+            channelName: _channelName,
+            role: _role,
+          ),
+        ),
+      );
+    }
+  }
+
+  Future<void> _handleCameraAndMic() async {
+    await PermissionHandler().requestPermissions(
+      [PermissionGroup.camera, PermissionGroup.microphone],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: FlatButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            new MaterialPageRoute(
-              builder: (context) => IndexPage(),
+        body: Builder(
+      builder: (context) => Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FlatButton(
+              onPressed: () {
+                setState(() {
+                  _channelName = 'Sports';
+                });
+              },
+              child: Text(
+                'Sports',
+              ),
             ),
-          );
-        },
-        child: Text(
-          'Click Me',
+            FlatButton(
+              child: Text('Join'),
+              onPressed: () {
+                _channelName.isEmpty
+                    ? Scaffold.of(context).showSnackBar(snackBar)
+                    : onJoin();
+              },
+            ),
+          ],
         ),
       ),
     ));
