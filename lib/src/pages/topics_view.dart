614b9e8abde30f3a6f312c2f0a961c23a5524ac9
Samuel Atefah <satefah1@jhu.edu>
2020-09-13 03:22:07 -0400

    implemented AddTopic widget

--- lib/src/pages/topics_view.dart
+++ lib/src/pages/topics_view.dart
@@ -1,6 +1,7 @@
 import 'package:agora_rtc_engine/rtc_engine.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:stay_connected/src/pages/Widgets/add_topic.dart';
 import 'package:stay_connected/src/pages/animals_view.dart';
 import 'package:stay_connected/src/pages/call.dart';
 import 'package:stay_connected/src/pages/index.dart';
@@ -113,26 +114,56 @@ class _TopicsViewState extends State<TopicsView> {
                       crossAxisCount: 2,
                     ),
                     children: [
-                      addTopic('Animals', AnimalsView(),
-                          'https://lh3.googleusercontent.com/9alLmIWwWkzfP-q-z08Ob41138rgK-ptBfqCqBoFyh4mphKfw1pEFuJOQKH5ksKhQlQUIlct1g=w640-h400-e365-rj-sc0x00ffffff'),
-                      addTopic('Art', ArtView(),
-                          'https://s3-eu-west-1.amazonaws.com/wbm.thumbnail/dissolve/1200/941126.jpg'),
-                      addTopic('Beauty', BeautyView(),
-                          'https://post.healthline.com/wp-content/uploads/2020/04/makeup_composition_overhead-1200x628-facebook-1200x628.jpg'),
-                      addTopic('Books', BooksView(),
-                          'https://www.rd.com/wp-content/uploads/2017/10/This-Is-How-Long-It-Takes-To-Read-The-Whole-Dictionary_509582812-Billion-Photos_FB-e1574101045824.jpg'),
-                      addTopic('Cooking', CookingView(),
-                          'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQWYrBDnEtKo-YEIHoXp-pQwuuAq4-kAFiHVw&usqp=CAU'),
-                      addTopic('Games', GamesView(),
-                          'https://i.pcmag.com/imagery/articles/00n0I7NojThD7LNdz31s0jH-8.fit_scale.size_1050x591.v1569486931.jpg'),
-                      addTopic('General', GeneralView(),
-                          'https://proofthatblog.com/wp-content/uploads/2013/06/question-mark.jpg'),
-                      addTopic('Movies/TV', Movies_TVView(),
-                          'https://cdn3.movieweb.com/i/article/M4dH9tdBE4BmfPWnS3RerbK8sOOqMA/1200:100/Disney-Plus-Launch-Movies-Tv-Shows-Full-List.jpg'),
-                      addTopic('Music', MusicView(),
-                          'https://cnet1.cbsistatic.com/img/FaRwLPEhtWImd3ABytDiyJz7HQM=/1200x630/2020/06/16/91844037-74e7-4ed8-a9cb-22fad4d8a7ce/beats-solo3-club-collection.jpg'),
-                      addTopic('Sports', SportsView(),
-                          'https://media.npr.org/assets/img/2020/06/10/gettyimages-200199027-001-b5fb3d8d8469ab744d9e97706fa67bc5c0e4fa40.jpg'),
+                      AddTopic(
+                          'Animals',
+                          AnimalsView(),
+                          'https://lh3.googleusercontent.com/9alLmIWwWkzfP-q-z08Ob41138rgK-ptBfqCqBoFyh4mphKfw1pEFuJOQKH5ksKhQlQUIlct1g=w640-h400-e365-rj-sc0x00ffffff',
+                          false),
+                      AddTopic(
+                          'Art',
+                          null,
+                          'https://s3-eu-west-1.amazonaws.com/wbm.thumbnail/dissolve/1200/941126.jpg',
+                          false),
+                      AddTopic(
+                          'Beauty',
+                          null,
+                          'https://post.healthline.com/wp-content/uploads/2020/04/makeup_composition_overhead-1200x628-facebook-1200x628.jpg',
+                          false),
+                      AddTopic(
+                          'Books',
+                          null,
+                          'https://www.rd.com/wp-content/uploads/2017/10/This-Is-How-Long-It-Takes-To-Read-The-Whole-Dictionary_509582812-Billion-Photos_FB-e1574101045824.jpg',
+                          false),
+                      AddTopic(
+                          'Cooking',
+                          null,
+                          'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQWYrBDnEtKo-YEIHoXp-pQwuuAq4-kAFiHVw&usqp=CAU',
+                          false),
+                      AddTopic(
+                          'Games',
+                          null,
+                          'https://i.pcmag.com/imagery/articles/00n0I7NojThD7LNdz31s0jH-8.fit_scale.size_1050x591.v1569486931.jpg',
+                          false),
+                      AddTopic(
+                          'General',
+                          null,
+                          'https://proofthatblog.com/wp-content/uploads/2013/06/question-mark.jpg',
+                          false),
+                      AddTopic(
+                          'Movies/TV',
+                          null,
+                          'https://cdn3.movieweb.com/i/article/M4dH9tdBE4BmfPWnS3RerbK8sOOqMA/1200:100/Disney-Plus-Launch-Movies-Tv-Shows-Full-List.jpg',
+                          false),
+                      AddTopic(
+                          'Music',
+                          null,
+                          'https://cnet1.cbsistatic.com/img/FaRwLPEhtWImd3ABytDiyJz7HQM=/1200x630/2020/06/16/91844037-74e7-4ed8-a9cb-22fad4d8a7ce/beats-solo3-club-collection.jpg',
+                          false),
+                      AddTopic(
+                          'Sports',
+                          null,
+                          'https://media.npr.org/assets/img/2020/06/10/gettyimages-200199027-001-b5fb3d8d8469ab744d9e97706fa67bc5c0e4fa40.jpg',
+                          false),
                     ],
                   ),
                 ),
