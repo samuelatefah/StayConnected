fba2a6c830ad473aea44be5dc03a284d6fdb25af
Samuel Atefah <satefah1@jhu.edu>
2020-09-12 21:24:44 -0400

    created topics_view.dart

---
+++ lib/src/pages/topics_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TopicsView extends StatefulWidget {
+  @override
+  _TopicsViewState createState() => _TopicsViewState();
+}
+
+class _TopicsViewState extends State<TopicsView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
